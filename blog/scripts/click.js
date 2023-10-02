69b5d9792ec47185cd056752b2af80184dcb7138
estella00911 <estella00911@gmail.com>
2023-10-01 23:54:42 -0500

    feat: edit directory

--- blog/scripts/click.js
+++
@@ -1,8 +0,0 @@
-const urlBase = 'https://gentle-depths-67267.herokuapp.com'
-const articlePost = document.querySelectorAll('.article__post')
-for (let i = 0; i < articlePost.length; i++) {
-  articlePost[i].addEventListener('click', (e) => {
-    location.href = `${urlBase}/article/${e.currentTarget.id}`
-    console.log(`${urlBase}/article/${e.currentTarget.id}`)
-  })
-}
